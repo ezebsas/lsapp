00b194f3015e396ac45be8761e8e2773bd28224f
Ezequiel Mollinedo <mollinedoezequiel@gmail.com>
2017-10-23 10:13:08 -0300

    Agrego lo que tengo hasta ahora

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-inverse">
+  <div class="container">
+    <div class="navbar-header"> 
+      <a href="/" class="navbar-brand">{{config('app.name', 'lsApp')}}</a> 
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="/posts">Blog</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+/*
+Route::get('/hello', function () {
+    return '<h1>Hello world!</h1>';
+});
+
+Route::get('/about', function(){
+	return view('pages/about');
+});
+
+Route::get('/users/{id}/{name}', function($id, $name){
+	return 'This user is '.$name.' whith id '.$id;
+});
+
+Route::get('/', 'function () {
+    return view('welcome');
+}');
+
+*/
+
+Route::get('/', 'PagesController@index');
+
+Route::get('/about', 'PagesController@about');
+
+Route::get('/services', 'PagesController@services');
+
+Route::resource('posts', 'PostsController');
