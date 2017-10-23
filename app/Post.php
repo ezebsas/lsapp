cc051f0e983548a2a5f6ab5031d15ba653f90e32
Ezequiel Mollinedo <mollinedoezequiel@gmail.com>
2017-10-23 16:00:49 -0300

    User Authentication and Access Control

--- app/Post.php
+++ app/Post.php
@@ -11,5 +11,10 @@ class Post extends Model
     //Primary Key
     public $primaryKey = 'id';
     //Timestamps
-    public $timestamps = true;
+    public $timestamps = true;        
+
+
+    public function user(){
+    	return $this->belongsTo('App\User');
+    }
 }
